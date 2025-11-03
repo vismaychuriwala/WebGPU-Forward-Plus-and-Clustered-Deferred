8487f867503166d988bc161539462a33cafcf9d3
vismaychuriwala <vismaychuriwala@gmail.com>
2025-11-03 01:46:10 -0500

    Adding favicon and website name

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ function setRenderer(mode: string) {
 }
 
 const renderModes = { naive: 'naive', forwardPlus: 'forward+', clusteredDeferred: 'clustered deferred' };
-let renderModeController = gui.add({ mode: renderModes.naive }, 'mode', renderModes);
+let renderModeController = gui.add({ mode: renderModes.clusteredDeferred }, 'mode', renderModes);
 renderModeController.onChange(setRenderer);
 
 setRenderer(renderModeController.getValue());
